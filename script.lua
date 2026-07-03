b75bb11d8f28235227417a94f7bebedffb7ef98f
bkscripts-pro <bkscripts77@gmail.com>
2026-07-02 23:39:53 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,11 +1,29 @@
-
+-- BK 
 local url = "http://127.0.0.1:8765/script"
+local headers = {["Authorization"] = "Bearer BK_SECRET_TOKEN_X7"}
+
 local sucesso, resposta = pcall(function()
-    return game:HttpGet(url, true)
+    -
+    if syn and syn.request then
+        local r = syn.request({Url = url, Method = "GET", Headers = headers})
+        return r.Body
+    elseif http_request then
+        local r = http_request({Url = url, Method = "GET", Headers = headers})
+        return r.Body
+    elseif game and game.HttpGet then
+        return game:HttpGet(url, true)
+    else
+        error("Nenhum método HTTP disponível neste executor.")
+    end
 end)
 
-if sucesso and resposta then
-    loadstring(resposta)()
+if sucesso and resposta and resposta ~= "" then
+    local f, err = loadstring(resposta)
+    if f then
+        f()
+    else
+        warn("[BK LOADER] Erro no script: " .. tostring(err))
+    end
 else
-    warn("[BK LOADER] Servidor ADM BK não está ativo. Ligue o aplicativo e tente novamente.")
+    warn("[BK LOADER] Falha ao obter script. Verifique se o ADM BK está ativo.")
 end
